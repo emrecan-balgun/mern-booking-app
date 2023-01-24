ec3ecd35a7cddb4f912962fcfa8022f8d5aafd62
Emrecan Balgün <deathlygrave@gmail.com>
2023-01-24 16:15:16 +0300

    final header

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
 import "./home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
 }
 
 export default Home;
