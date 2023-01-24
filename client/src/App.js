f39d877568e3484f4e50964c6fa1a4992ea09b06
Emrecan Balgün <deathlygrave@gmail.com>
2023-01-24 14:59:37 +0300

    create pages and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/hotels",
+    element: <List />,
+  },
+  {
+    path: "/hotels/:id",
+    element: <Hotel />,
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
