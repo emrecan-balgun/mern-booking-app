534413ad5a33e2a5c5498d7d30fc65602be2825e
Emrecan Balgün <deathlygrave@gmail.com>
2023-01-24 17:04:33 +0300

    add home page items

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
+import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
 
 function Home() {
@@ -8,6 +11,13 @@ function Home() {
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+      </div>
     </div>
   );
 }
